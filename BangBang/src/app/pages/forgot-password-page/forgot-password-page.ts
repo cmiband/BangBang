e9082ec1efddf3249e075dc46e0c5d3015be4b28
xKsOm <adam.domzalowicz@gmail.com>
2025-11-20 17:02:15 +0100

    email page

--- BangBang/src/app/pages/forgot-password-page/forgot-password-page.ts
+++ BangBang/src/app/pages/forgot-password-page/forgot-password-page.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
-import { CustomButton } from '../../shared/custom-button/custom-button';
-import { CustomTextInput } from '../../shared/custom-text-input/custom-text-input';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { Auth } from '../../auth/auth';
 
 @Component({
   selector: 'app-forgot-password-page',
-  imports: [CustomButton, CustomTextInput,RouterLink],
+  imports: [RouterLink, FormsModule, NgIf],
   templateUrl: './forgot-password-page.html',
   styleUrl: './forgot-password-page.css'
 })
 export class ForgotPasswordPage {
+  email = '';
+  error = '';
+  success = '';
+  username = '';
+  password = '';
 
+  constructor(private authService: Auth, private router: Router) {}
+
+  onSubmit() {
+    this.username=''
+    const users = this.authService.returnUsers()
+    for(let user of users) {
+      if (user.email == this.email) {
+        this.username = user.username
+        this.password = user.password
+      }
+    } 
+    if (this.username === '') {
+      this.error = 'error'
+      this.success = ''
+    }
+    else {
+      this.error = ''
+      this.success = 'true'
+    }
+  }
 }
